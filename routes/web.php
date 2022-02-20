a50b0053093580069d841e76d059a6beeafbc470
ahmad alfaruq <ahmadalfaruq098@gmail.com>
2022-02-20 16:31:18 +0700

    praktikum1 routing web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function ($id) {
+    echo "Selamat Datang";
+});
+Route::get('/articeles/{id}', function ($id) {
+    return "ini merupakan halaman artikel dengan $id";
+});
+
+
+
